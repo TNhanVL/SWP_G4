ae2c144feac1a7390677b402777fb0553110266b
TTNhan <nhan12341184@gmail.com>
2023-10-13 20:49:38 +0700

    add AdminDAO

---
+++ src/main/java/com/swp_project_g4/Database/AdminDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Service.MD5;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Thanh Duong
+ */
+public class AdminDAO extends DBConnection {
+
+
+    public static void main(String[] args) throws ClassNotFoundException {
+//        System.out.println(getAllUsers().size());
+    }
+}
