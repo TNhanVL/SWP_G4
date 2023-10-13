7cb4241aeab740c841a87905a3b6d2ed8df0e6d1
TTNhan <nhan12341184@gmail.com>
2023-10-13 20:50:27 +0700

    add checkAdmin method

--- src/main/java/com/swp_project_g4/Database/AdminDAO.java
+++ src/main/java/com/swp_project_g4/Database/AdminDAO.java
@@ -41,6 +41,50 @@ public class AdminDAO extends DBConnection {
         return ok;
     }
 
+    /**
+     * check password of admin
+     *
+     * @param username
+     * @param password
+     * @param hashed true if password hashed
+     * @return 0 - ok; 1 - not exist; 2 - incorrect pw
+     */
+    public static int checkAdmin(String username, String password, boolean hashed) {
+        int status = -1;
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select password from [admin] where username = ?");
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+
+            //not exist
+            if (!resultSet.next()) {
+                status = 1;
+            } else {
+                String pw = resultSet.getString("password");
+                if (!hashed) {
+                    password = MD5.getMd5(password);
+                }
+                if (pw.equals(password)) {
+                    //correct
+                    status = 0;
+                } else {
+                    //not correct password
+                    status = 2;
+                }
+            }
+
+            //disconnect to database
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //return result
+        return status;
+    }
+
     public static void main(String[] args) throws ClassNotFoundException {
 //        System.out.println(getAllUsers().size());
     }
