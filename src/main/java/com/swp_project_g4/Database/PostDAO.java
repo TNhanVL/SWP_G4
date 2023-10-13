733beb50c721d66f3b3d180ec09db3dddc8224ed
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-13 22:19:18 +0700

    add PostDAO

---
+++ src/main/java/com/swp_project_g4/Database/PostDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Model.Post;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Thanh Duong
+ */
+public class PostDAO extends DBConnection {
+
+    
+
+  
+    public static void main(String[] args) {
+//        System.out.println(getPost(1));
+//        Post p = getPost(3);
+//        p.setContent("Hmm...");
+//        updatePost(p);
+//        deletePost(3);
+
+    }
+}
