bd3ca041e13f1fd487d972366e3b5635753ee457
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-13 22:40:29 +0700

    add exist anf get Question

--- src/main/java/com/swp_project_g4/Database/QuestionDAO.java
+++ src/main/java/com/swp_project_g4/Database/QuestionDAO.java
@@ -17,7 +17,115 @@ import java.util.logging.Logger;
  */
 public class QuestionDAO extends DBConnection {
 
-    
+    public static boolean existQuestion(int questionID) {
+        boolean ok = false;
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select questionID from question where questionID = ?");
+            statement.setInt(1, questionID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                if (resultSet.getInt("questionID") == questionID) {
+                    ok = true;
+                }
+            }
+
+            //disconnect to database
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //return result
+        return ok;
+    }
+
+    public static Question getQuestion(int questionID) {
+        Question question = null;
+
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select * from question where questionID = ?");
+            statement.setInt(1, questionID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                question = new Question(
+                        resultSet.getInt("questionID"),
+                        resultSet.getInt("lessonID"),
+                        resultSet.getInt("index"),
+                        resultSet.getString("content"),
+                        resultSet.getInt("type"),
+                        resultSet.getInt("point")
+                );
+            }
+
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return question;
+    }
+
+    public static ArrayList<Question> getQuestionByLessonID(int lessonID) {
+        ArrayList<Question> questions = new ArrayList<>();
+
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select * from question where lessonID = ? order by [index]");
+            statement.setInt(1, lessonID);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Question question = new Question(
+                        resultSet.getInt("questionID"),
+                        resultSet.getInt("lessonID"),
+                        resultSet.getInt("index"),
+                        resultSet.getString("content"),
+                        resultSet.getInt("type"),
+                        resultSet.getInt("point")
+                );
+                questions.add(question);
+            }
+
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return questions;
+    }
+
+    public static int getNumberQuestionByLessonID(int lessonID) {
+        int ans = 0;
+
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select count(*) as number from question where lessonID = ?");
+            statement.setInt(1, lessonID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt("number");
+            }
+
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return ans;
+    }
+
     
 
     public static void main(String[] args) {
