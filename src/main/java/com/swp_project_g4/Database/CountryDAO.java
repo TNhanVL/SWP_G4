3d4d0096fd6995b11bfb6738053f29cd07586654
Dylanruan121003 <huongntdce171360@fpt.edu.vn>
2023-10-13 21:39:19 +0700

    add CountryDAO

---
+++ src/main/java/com/swp_project_g4/Database/CountryDAO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Model.Country;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Gr4
+ */
+public class CountryDAO extends DBConnection {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println(getAllCountry());
+    }
+
+}
