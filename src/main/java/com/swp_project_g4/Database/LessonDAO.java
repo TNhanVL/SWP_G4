291a72c8441b8ddd1aeb85d981a9c235105f7539
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-13 21:30:19 +0700

    add lessonDAO

---
+++ src/main/java/com/swp_project_g4/Database/LessonDAO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Model.Lesson;
+import com.swp_project_g4.Model.Chapter;
+import com.swp_project_g4.Model.QuizResult;
+import com.swp_project_g4.Service.Certificate;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Thanh Duong
+ */
+public class LessonDAO extends DBConnection {
+
+   
+
+    
+
+    
+   
+    
+
+    
+
+    
+    
+
+    
+
+    
+
+    
+
+    
+    
+
+    public static void main(String[] args) {
+        System.out.println(getNumberLessonsByChapterID(1));
+    }
+}
