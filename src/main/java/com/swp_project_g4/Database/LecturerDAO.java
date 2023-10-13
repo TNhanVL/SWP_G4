ead77c16802c65ea320825463659f8e77c09873e
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-13 21:19:48 +0700

    add LecturerDAO

---
+++ src/main/java/com/swp_project_g4/Database/LecturerDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Model.Lecturer;
+import com.swp_project_g4.Model.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Thanh Duong
+ */
+public class LecturerDAO extends DBConnection {
+
+    
+}
