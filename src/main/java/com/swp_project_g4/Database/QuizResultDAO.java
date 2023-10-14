6817552e4aadf2e4617aab110bf72d7762e01916
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-14 19:23:01 +0700

    add exist and getQuizResultPoint method

--- src/main/java/com/swp_project_g4/Database/QuizResultDAO.java
+++ src/main/java/com/swp_project_g4/Database/QuizResultDAO.java
@@ -19,7 +19,80 @@ import java.util.logging.Logger;
  */
 public class QuizResultDAO extends DBConnection {
 
-   
+    public static boolean existQuizResult(int ID) {
+        boolean ok = false;
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select ID from quizResult where ID = ?");
+            statement.setInt(1, ID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                if (resultSet.getInt("ID") == ID) {
+                    ok = true;
+                }
+            }
+
+            //disconnect to database
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //return result
+        return ok;
+    }
+
+    public static QuizResult getQuizResult(int ID) {
+        QuizResult quizResult = null;
+
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("select * from quizResult where ID = ?");
+            statement.setInt(1, ID);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                quizResult = new QuizResult(
+                        resultSet.getInt("ID"),
+                        resultSet.getInt("lessonID"),
+                        resultSet.getInt("userID"),
+                        resultSet.getTimestamp("startTime"),
+                        resultSet.getTimestamp("endTime")
+                );
+            }
+
+            disconnect();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return quizResult;
+    }
+
+    public static int getQuizResultPoint(int ID) {
+        QuizResult quizResult = getQuizResult(ID);
+
+        if (quizResult == null) {
+            return 0;
+        }
+
+        ArrayList<Question> questions = QuestionDAO.getQuestionByLessonID(quizResult.getLessonID());
+
+        int point = 0;
+
+        for (Question question : questions) {
+            if (ChosenAnswerDAO.CheckChosenAnswerCorrect(quizResult.getID(), question.getQuestionID())) {
+                point++;
+            }
+        }
+
+        return point;
+    }
+
   
    
     public static void main(String[] args) {
