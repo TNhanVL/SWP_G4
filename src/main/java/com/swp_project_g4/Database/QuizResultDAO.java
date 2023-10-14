7881205d3004ef99ac33122052acb23722b960f5
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-14 19:21:51 +0700

    QuizResultDAO

---
+++ src/main/java/com/swp_project_g4/Database/QuizResultDAO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Model.Question;
+import com.swp_project_g4.Model.QuizResult;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Thanh Duong
+ */
+public class QuizResultDAO extends DBConnection {
+
+   
+  
+   
+    public static void main(String[] args) {
+
+    }
+}
