92e5f7ea1da4e6177fae1982f736ade36eb46c1c
Dylanruan121003 <huongntdce171360@fpt.edu.vn>
2023-10-16 13:34:46 +0700

    add ChapterDAO

---
+++ src/main/java/com/swp_project_g4/Database/ChapterDAO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import com.swp_project_g4.Model.Chapter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Thanh Duong
+ */
+public class ChapterDAO extends DBConnection {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println(getChaptersByCourseID(1));
+    }
+}
