9369ea12f68767b7066f8c83c12d3a0febd29acb
Dylanruan121003 <huongntdce171360@fpt.edu.vn>
2023-10-16 13:41:40 +0700

    add ChosenAnswerDAO

---
+++ src/main/java/com/swp_project_g4/Database/ChosenAnswerDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp_project_g4.Database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Gr4
+ */
+public class ChosenAnswerDAO extends DBConnection {
+
+    
+
+    public static void main(String[] args) {
+        deleteChosenAnswerOfQuestion(1, 3);
+    }
+}
