c39090941be62f6afc181edb99854a2501f73ab0
thientai1223 <anhvlce171612@fpt.edu.vn>
2023-10-21 23:58:03 +0700

    update ReviewDAO

--- src/main/java/com/swp_project_g4/Database/ReviewDAO.java
+++ src/main/java/com/swp_project_g4/Database/ReviewDAO.java
@@ -11,30 +11,30 @@ import java.util.logging.Logger;
 public class ReviewDAO extends DBConnection {
 
 
-//    public static boolean insertReview(Review review) {
-//        boolean result = false;
-//        try {
-//            //connect to database
-//            connect();
-//
-//            statement = conn.prepareStatement("insert into review(userID, courseID, reviewed, verified, note) values (?,?,?,?,?)");
-////            statement.setInt(1,Review.getReviewID());
-//            statement.setInt(1, review.getUserID());
-//            statement.setInt(2, review.getCourseID());
-//            statement.setBoolean(3, review.isReviewed());
-//            statement.setBoolean(4, review.isVerified());
-//            statement.setString(5, review.getNote());
-//            statement.execute();
-//            //Indentify the last ID inserted
-//            //disconnect to database
-//            disconnect();
-//
-//            result = true;
-//        } catch (SQLException | ClassNotFoundException ex) {
-//            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return result;
-//    }
+    public static boolean insertReview(Review review) {
+        boolean result = false;
+        try {
+            //connect to database
+            connect();
+
+            statement = conn.prepareStatement("insert into review(userID, courseID, reviewed, verified, note) values (?,?,?,?,?)");
+//            statement.setInt(1,Review.getReviewID());
+            statement.setInt(1, review.getID());
+            statement.setInt(2, review.getCourseID());
+            statement.setBoolean(3, review.isReviewed());
+            statement.setBoolean(4, review.isVerified());
+            statement.setString(5, review.getNote());
+            statement.execute();
+            //Indentify the last ID inserted
+            //disconnect to database
+            disconnect();
+
+            result = true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
 
 
     public static boolean updateReview(Review review) {
