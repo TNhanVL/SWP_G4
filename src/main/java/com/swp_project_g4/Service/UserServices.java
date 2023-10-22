a1610a0159ea55b261fff57e6ffaaffaf7ca3e14
TTNhan <nhan12341184@gmail.com>
2023-10-22 15:27:21 +0700

    add userServices, isValidInformation

---
+++ src/main/java/com/swp_project_g4/Service/UserServices.java
@@ -0,0 +1,8 @@
+package com.swp_project_g4.Service;
+
+public class UserServices {
+    public static boolean isValidInformation(String name, String phone_number, String email, String birthday) {
+
+        return true;
+    }
+}
