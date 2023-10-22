ca4e32c36b0efff9762954c05263e29c75e1b5c8
TTNhan <nhan12341184@gmail.com>
2023-10-22 20:33:54 +0700

    add testcase addCourse

--- src/test/java/com/swp_project_g4/Database/CourseDAOTest.java
+++ src/test/java/com/swp_project_g4/Database/CourseDAOTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CourseDAOTest {
 
-// Test the search course function
+    // Test the search course function
     @Test
     void searchCoursesWithEmptyName() {
         CourseDAO courseDAO = new CourseDAO();
@@ -40,7 +40,7 @@ public class CourseDAOTest {
     }
 
     @Test
-    void searchCoursesWithPartialName(){
+    void searchCoursesWithPartialName() {
         CourseDAO courseDAO = new CourseDAO();
         String name = "Java";
         ArrayList<Course> courses = courseDAO.searchCourses(name);
@@ -54,5 +54,154 @@ public class CourseDAOTest {
         }
     }
 
+    @Test
+    void addCourse_test_1() {
+        int organizationID = 1;
+        String name = "";
+        String picture = "";
+        double price = 0;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_2() {
+        int organizationID = 1;
+        String name = "Nihongo";
+        String picture = "";
+        double price = 0;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_3() {
+        int organizationID = 1;
+        String name = "Nihongo";
+        String picture = "a.png";
+        double price = 10.234;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_4() {
+        int organizationID = 1;
+        String name = "Nihongo";
+        String picture = "a.png";
+        double price = -10.12;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_5() {
+        int organizationID = 1;
+        String name = "PYTHON FOR BEGINNER";
+        String picture = "a.png";
+        double price = 4;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_6() {
+        int organizationID = 1;
+        String name = "PYTHON FOR BEGINNER";
+        String picture = "https://www.vitto.vn/sac-hoa-tulip";
+        double price = 4;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_7() {
+        int organizationID = 1;
+        String name = "Nihongo";
+        String picture = "a.png";
+        double price = 4;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_8() {
+        int organizationID = 1;
+        String name = "Nihongo";
+        String picture = "https://www.vitto.vn/sac-hoa-tulip";
+        double price = 4;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_9() {
+        int organizationID = 1;
+        String name = "PYTHON FOR BEGINNER";
+        String picture = "https://www.vitto.vn/sac-hoa-tulip";
+        double price = 10.234;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(true, res);
+    }
+
+    @Test
+    void addCourse_test_10() {
+        int organizationID = -12;
+        String name = "Nihongo";
+        String picture = "a.png";
+        double price = 4;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_11() {
+        int organizationID = -12;
+        String name = "";
+        String picture = "";
+        double price = 0;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_12() {
+        int organizationID = -12;
+        String name = "PYTHON FOR BEGINNER";
+        String picture = "https://www.vitto.vn/sac-hoa-tulip";
+        double price = 10.234;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_13() {
+        int organizationID = 1607;
+        String name = "";
+        String picture = "";
+        double price = -10.12;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_14() {
+        int organizationID = 1607;
+        String name = "Nihongo";
+        String picture = "a.png";
+        double price = 10.234;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
+
+    @Test
+    void addCourse_test_15() {
+        int organizationID = 1607;
+        String name = "PYTHON FOR BEGINNER";
+        String picture = "https://www.vitto.vn/sac-hoa-tulip";
+        double price = 4;
+        boolean res = CourseDAO.addCourse(organizationID, name, picture, price);
+        assertEquals(false, res);
+    }
 }
 
