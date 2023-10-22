35f08834817d8522bdcb2ca27ed97be06d4de3e7
Dylanruan121003 <huongntdce171360@fpt.edu.vn>
2023-10-22 14:05:43 +0700

    something

--- src/test/java/com/swp_project_g4/Database/UserDAOTest.java
+++ src/test/java/com/swp_project_g4/Database/UserDAOTest.java
@@ -37,4 +37,59 @@ class UserDAOTest {
         boolean res = UserDAO.checkUserPassword("dylan12", "", true);
         assertEquals(res, false);
     }
+
+    @Test
+    void checkUserPassword_test_6() {
+        boolean res = UserDAO.checkUserPassword("dylan12", "a", false);
+        assertEquals(res, false);
+    }
+
+    @Test
+    void checkUserPassword_test_7() {
+        boolean res = UserDAO.checkUserPassword("dylan12", "123456", false);
+        assertEquals(res, true);
+    }
+
+    @Test
+    void checkUserPassword_test_8() {
+        boolean res = UserDAO.checkUserPassword("dylan12", "e10adc3949ba59abbe56e057f20f883e", true);
+        assertEquals(res, true);
+    }
+
+    @Test
+    void checkUserPassword_test_9() {
+        boolean res = UserDAO.checkUserPassword("ttnhan", "", false);
+        assertEquals(res, false);
+    }
+
+    @Test
+    void checkUserPassword_test_10() {
+        boolean res = UserDAO.checkUserPassword("ttnhan", "a", true);
+        assertEquals(res, false);
+    }
+
+    @Test
+    void checkUserPassword_test_11() {
+        boolean res = UserDAO.checkUserPassword("ttnhan", "a", false);
+        assertEquals(res, true);
+    }
+
+    @Test
+    void checkUserPassword_test_12() {
+        boolean res = UserDAO.checkUserPassword("ttnhan", "123456", true);
+        assertEquals(res, false);
+    }
+
+    @Test
+    void checkUserPassword_test_13() {
+        boolean res = UserDAO.checkUserPassword("ttnhan", "0cc175b9c0f1b6a831c399e269772661", true);
+        assertEquals(res, true);
+    }
+
+    @Test
+    void checkUserPassword_test_14() {
+        boolean res = UserDAO.checkUserPassword("ttnhan", "0cc175b9c0f1b6a831c399e269772661", false);
+        assertEquals(res, false);
+    }
+
 }
