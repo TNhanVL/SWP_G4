c7005e9ea2461bb6a017f2b24870201acdf8363a
TTNhan <nhan12341184@gmail.com>
2023-10-22 16:43:37 +0700

    add check phone number

--- src/main/java/com/swp_project_g4/Service/UserServices.java
+++ src/main/java/com/swp_project_g4/Service/UserServices.java
@@ -1,6 +1,7 @@
 package com.swp_project_g4.Service;
 
 import com.swp_project_g4.Database.DBConnection;
+import com.swp_project_g4.Database.UserDAO;
 import com.swp_project_g4.Model.User;
 
 import java.sql.SQLException;
@@ -12,11 +13,19 @@ public class UserServices {
         try {
             boolean fineName = name.matches("^[\\p{L}\\p{M}]+([\\p{L}\\p{Pd}\\p{Zs}'.]*[\\p{L}\\p{M}])+$|^[\\p{L}\\p{M}]+$");
             if(!fineName){
+                Logger.getLogger(UserDAO.class.getName()).log(Level.INFO, "incorrect name");
+                return false;
+            }
+
+            boolean finePhoneNumber = phone_number.matches("^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$");
+            if(!finePhoneNumber){
+                Logger.getLogger(UserDAO.class.getName()).log(Level.INFO, "incorrect phone number");
                 return false;
             }
         } catch (Exception e) {
 
         }
+        Logger.getLogger(UserDAO.class.getName()).log(Level.INFO, "success");
         return true;
     }
 }
