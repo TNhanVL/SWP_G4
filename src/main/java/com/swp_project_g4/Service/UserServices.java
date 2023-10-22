f71bb495fb068947010340d6609cb60d82d04e02
TTNhan <nhan12341184@gmail.com>
2023-10-22 15:53:03 +0700

    change Date to Datetime

--- src/main/java/com/swp_project_g4/Service/UserServices.java
+++ src/main/java/com/swp_project_g4/Service/UserServices.java
@@ -1,8 +1,19 @@
 package com.swp_project_g4.Service;
 
+import com.swp_project_g4.Database.DBConnection;
+import com.swp_project_g4.Model.User;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class UserServices {
     public static boolean isValidInformation(String name, String phone_number, String email, String birthday) {
+        try {
 
+        } catch (Exception e) {
+
+        }
         return true;
     }
 }
