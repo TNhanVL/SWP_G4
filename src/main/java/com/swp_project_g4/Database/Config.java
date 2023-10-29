be7cd2ba00d8ed9c5cff24f54930e995df9088c5
potasium142 <potasium.1024@gmail.com>
2023-10-29 19:16:43 +0700

    oopise wossy

--- src/main/java/com/swp_project_g4/Database/Config.java
+++
@@ -1,9 +0,0 @@
-package com.swp_project_g4.Database;
-
-public interface Config {
-    String SERVER = "172.22.156.224";
-    String USER = "SA";
-    String PASSWORD = "a";
-    String DATABASE_NAME = "DB_SWP_Project_G4";
-    int PORT = 1433;
-}
