bacf2de58182fbba92e0cb5f4288613f9f2493fa
TTNhan <nhan12341184@gmail.com>
2023-10-29 12:23:22 +0700

    add ScheduledTasks

--- src/main/java/com/swp_project_g4/SwpProjectG4Application.java
+++ src/main/java/com/swp_project_g4/SwpProjectG4Application.java
@@ -2,7 +2,9 @@ package com.swp_project_g4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SwpProjectG4Application {
 
